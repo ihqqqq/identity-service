fc92eba70b220af77f80081a18ceae57ff6a3387
phi <phitv3726@ut.edu.vn>
2024-09-28 20:18:34 +0700

    first commit

---
+++ src/main/java/com/ihqqq/identity_service/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.ihqqq.identity_service.controller;
+
+import com.ihqqq.identity_service.dto.request.UserCreationRequest;
+import com.ihqqq.identity_service.dto.request.UserUpdateRequest;
+import com.ihqqq.identity_service.entity.User;
+import com.ihqqq.identity_service.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    User createUser(@RequestBody @Valid UserCreationRequest request) {
+        return userService.createUser(request);
+    }
+
+    @GetMapping
+    List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable String userId) {
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/{userId}")
+    User updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request) {
+        return userService.updateUser(userId, request);
+    }
+
+    @DeleteMapping("/{userId}")
+    String deleteUser(@PathVariable String userId) {
+        userService.deleteUser(userId);
+        return "User has been deleted";
+    }
+}
---
+++ src/main/java/com/ihqqq/identity_service/service/UserService.java
@@ -0,0 +1,54 @@
+package com.ihqqq.identity_service.service;
+
+import com.ihqqq.identity_service.dto.request.UserCreationRequest;
+import com.ihqqq.identity_service.dto.request.UserUpdateRequest;
+import com.ihqqq.identity_service.entity.User;
+import com.ihqqq.identity_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(UserCreationRequest request) {
+        User user = new User();
+
+        if(userRepository.existsByUsername(request.getUsername()))
+            throw new RuntimeException("User existed.");
+
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public User updateUser(String userId, UserUpdateRequest request) {
+        User user = getUser(userId);
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public void deleteUser(String userId) {
+        userRepository.deleteById(userId);
+    }
+}

ca5c7191c3570cb7f698096181ccac63931dfaf3
phi <phitv3726@ut.edu.vn>
2024-09-29 18:05:39 +0700

    Reapply "handle exception and normalize api response"
    This reverts commit 4cb77fa1af87b6ba068788b2c6fc73b221c5b3f3.

--- src/main/java/com/ihqqq/identity_service/controller/UserController.java
+++ src/main/java/com/ihqqq/identity_service/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.ihqqq.identity_service.controller;
 
+import com.ihqqq.identity_service.dto.request.ApiResponse;
 import com.ihqqq.identity_service.dto.request.UserCreationRequest;
 import com.ihqqq.identity_service.dto.request.UserUpdateRequest;
 import com.ihqqq.identity_service.entity.User;
@@ -17,8 +18,11 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    User createUser(@RequestBody @Valid UserCreationRequest request) {
-        return userService.createUser(request);
+    ApiResponse<User> createUser(@RequestBody @Valid UserCreationRequest request) {
+        ApiResponse<User> apiResponse = new ApiResponse<>();
+
+        apiResponse.setResult(userService.createUser(request));
+        return apiResponse;
     }
 
     @GetMapping
--- src/main/java/com/ihqqq/identity_service/service/UserService.java
+++ src/main/java/com/ihqqq/identity_service/service/UserService.java
@@ -3,6 +3,8 @@ package com.ihqqq.identity_service.service;
 import com.ihqqq.identity_service.dto.request.UserCreationRequest;
 import com.ihqqq.identity_service.dto.request.UserUpdateRequest;
 import com.ihqqq.identity_service.entity.User;
+import com.ihqqq.identity_service.exception.AppException;
+import com.ihqqq.identity_service.exception.ErrorCode;
 import com.ihqqq.identity_service.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,7 +20,7 @@ public class UserService {
         User user = new User();
 
         if(userRepository.existsByUsername(request.getUsername()))
-            throw new RuntimeException("User existed.");
+            throw new AppException(ErrorCode.USER_EXISTED);
 
         user.setUsername(request.getUsername());
         user.setPassword(request.getPassword());
